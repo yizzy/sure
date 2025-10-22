e7e85748e4493b675b500d2641981fc9b7a856f5
Juan José Mata <juanjo.mata@gmail.com>
2025-10-22 22:11:01 +0200

    Allow disabling AI assistant (#146)
    * Test disabling AI assistant
    
    * Leave "Maybe" out of it
    
    * It's "sure" now

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -21,9 +21,11 @@ class UsersController < ApplicationController
       @user.update!(user_params.except(:redirect_to, :delete_profile_image))
       @user.profile_image.purge if should_purge_profile_image?
 
-      # Add a special notice if AI was just enabled
+      # Add a special notice if AI was just enabled or disabled
       notice = if !was_ai_enabled && @user.ai_enabled
         "AI Assistant has been enabled successfully."
+      elsif was_ai_enabled && !@user.ai_enabled
+        "AI Assistant has been disabled."
       else
         t(".success")
       end
@@ -72,6 +74,8 @@ class UsersController < ApplicationController
         redirect_to goals_onboarding_path
       when "trial"
         redirect_to trial_onboarding_path
+      when "ai_prompts"
+        redirect_to settings_ai_prompts_path, notice: notice
       else
         redirect_to settings_profile_path, notice: notice
       end
---
+++ test/system/settings/ai_prompts_test.rb
@@ -0,0 +1,19 @@
+require "application_system_test_case"
+
+class Settings::AiPromptsTest < ApplicationSystemTestCase
+  setup do
+    @user = users(:family_admin)
+    @user.update!(ai_enabled: true)
+    login_as @user
+  end
+
+  test "user can disable ai assistant" do
+    visit settings_ai_prompts_path
+
+    click_button "Disable AI Assistant"
+
+    assert_current_path settings_ai_prompts_path
+    @user.reload
+    assert_not @user.ai_enabled?
+  end
+end
