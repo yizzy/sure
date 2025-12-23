104324a82b496b2a8a8748f77045f230b6c383b9
Carlos Adames <92492690+adamescj@users.noreply.github.com>
2025-12-23 19:55:23 -0400

    Pre-fill rule suggestion with transaction name and category (#497)
    Co-authored-by: Carlos Adames <cj@Carloss-MacBook-Air.local>

--- app/controllers/transaction_categories_controller.rb
+++ app/controllers/transaction_categories_controller.rb
@@ -11,7 +11,8 @@ class TransactionCategoriesController < ApplicationController
       flash[:cta] = {
         type: "category_rule",
         category_id: transaction.category_id,
-        category_name: transaction.category.name
+        category_name: transaction.category.name,
+        merchant_name: @entry.name
       }
     end
 
