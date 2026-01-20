45c4bc595b91408b5cfe2c47963ce6c1a169baa0
Juan José Mata <juanjo.mata@gmail.com>
2026-01-20 17:19:17 +0100

    Enable sending logs to Sentry
    Enabled logging to Sentry and patched Ruby logger.
    
    Signed-off-by: Juan José Mata <juanjo.mata@gmail.com>

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -5,6 +5,11 @@ if ENV["SENTRY_DSN"].present?
     config.breadcrumbs_logger = [ :active_support_logger, :http_logger ]
     config.enabled_environments = %w[production]
 
+    # Enable sending logs to Sentry
+    config.enable_logs = true
+    # Patch Ruby logger to forward logs
+    config.enabled_patches = [ :logger ]
+
     # Set traces_sample_rate to 1.0 to capture 100%
     # of transactions for performance monitoring.
     # We recommend adjusting this value in production.
