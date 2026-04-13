1aa3c8f0078265918b7a51f63a5419032afaec9f
Sure Admin (bot) <sure-admin@splashblot.com>
2026-04-13 14:52:46 +0200

    Fix parse_locale_float import via importmap (#1456)
    Add utils folder to importmap so bare specifiers resolve correctly.
    
    Root cause: PR #1176

--- config/importmap.rb
+++ config/importmap.rb
@@ -7,6 +7,7 @@ pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
 pin_all_from "app/components", under: "controllers", to: ""
 pin_all_from "app/javascript/services", under: "services", to: "services"
+pin_all_from "app/javascript/utils", under: "utils", to: "utils"
 pin "@github/hotkey", to: "@github--hotkey.js" # @3.1.1
 pin "@simonwep/pickr", to: "@simonwep--pickr.js" # @1.9.1
 
